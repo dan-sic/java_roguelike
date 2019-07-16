4fa5ca1580b78df91a05f7348d18e177272f72b5
Janek Pachnik <januzpachnik@gmailcom>
2019-07-16 13:40:51 +0200

    Impelemented items and display tiems on map

--- src/main/java/com/codecool/quest/Main.java
+++ src/main/java/com/codecool/quest/Main.java
@@ -46,7 +46,7 @@ public class Main extends Application {
         refresh();
         scene.setOnKeyPressed(this::onKeyPressed);
 
-        primaryStage.setTitle("Codecool Quest");
+        primaryStage.setTitle("Cool Quest");
         primaryStage.show();
     }
 
@@ -79,7 +79,10 @@ public class Main extends Application {
                 Cell cell = map.getCell(x, y);
                 if (cell.getActor() != null) {
                     Tiles.drawTile(context, cell.getActor(), x, y);
-                } else {
+                } else if(cell.getItem() != null){
+                    Tiles.drawTile(context, cell.getItem(), x, y);
+                }
+                else {
                     Tiles.drawTile(context, cell, x, y);
                 }
             }
