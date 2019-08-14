8c0282e18d6285fb055ec8c3784fb199ae9e25e0
Daniel_S <olgierd.dan@gmail.com>
2019-08-14 14:37:18 +0200

    Update skeleton movement

--- src/main/java/com/codecool/quest/logic/actors/monsters/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/actors/monsters/Skeleton.java
@@ -1,9 +1,20 @@
 package com.codecool.quest.logic.actors.monsters;
 
 import com.codecool.quest.logic.Cell;
-import java.util.concurrent.ThreadLocalRandom;
+import com.codecool.quest.logic.CellType;
+import com.codecool.quest.logic.CustomUtils;
 
 public class Skeleton extends Monster {
+
+    enum MoveDirection {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    private MoveDirection currentDirection;
+    private String moveType;
 
     public Skeleton(Cell cell) {
         super(cell);
@@ -11,26 +22,18 @@ public class Skeleton extends Monster {
         this.health = 10;
         this.attackPower = 5;
         setText(new String[]{"*Kling*","Please don't","I have a wife and family!"});
+
+        initiateRandomMovementDirection();
+
     }
 
     public void move() {
 
-        boolean isValidCoords = false;
-        Cell nextCell = null;
+        Cell nextCell = getNextCellDirectionBased();
 
-//        if skeleton is surrounded from every side > it may get stuck
-        while (!isValidCoords) {
-            int randomXPos = ThreadLocalRandom.current().nextInt(-1, 1 + 1);
-            int randomYPos = ThreadLocalRandom.current().nextInt(-1, 1 + 1);
+        checkPotentialCollision(nextCell);
 
-            nextCell = cell.getNeighbor(randomXPos, randomYPos);
-
-            boolean isMoveValid = isMoveValid(nextCell);
-
-            if (isMoveValid) {
-                isValidCoords = true;
-            }
-        }
+        nextCell = getNextCellDirectionBased();
 
         changeCell(nextCell);
     }
@@ -47,5 +50,59 @@ public class Skeleton extends Monster {
     @Override
     public String toString() {
         return "SKELETON";
+    }
+
+    private void initiateRandomMovementDirection() {
+        int randomChance = CustomUtils.getRandomNum(0, 10);
+
+        if(randomChance < 5) {
+            this.moveType = "horizontal";
+            this.currentDirection = MoveDirection.RIGHT;
+        } else {
+            this.moveType = "vertical";
+            this.currentDirection = MoveDirection.UP;
+        }
+    }
+
+    private void switchMovementDirection() {
+        switch(this.currentDirection) {
+            case DOWN:
+                this.currentDirection = MoveDirection.UP;
+                break;
+            case UP:
+                this.currentDirection = MoveDirection.DOWN;
+                break;
+            case LEFT:
+                this.currentDirection = MoveDirection.RIGHT;
+                break;
+            case RIGHT:
+                this.currentDirection = MoveDirection.LEFT;
+                break;
+        }
+    }
+
+    private Cell getNextCellDirectionBased() {
+        Cell nextCell;
+
+        if (this.currentDirection.equals(MoveDirection.DOWN)) {
+            nextCell = cell.getNeighbor(0,1);
+        } else if (this.currentDirection.equals(MoveDirection.UP)) {
+            nextCell = cell.getNeighbor(0,-1);
+        } else if (this.currentDirection.equals(MoveDirection.LEFT)) {
+            nextCell = cell.getNeighbor(-1,0);
+        } else {
+            nextCell = cell.getNeighbor(1,0);
+        }
+
+        return nextCell;
+    }
+
+    private void checkPotentialCollision(Cell nextCell) {
+        if (nextCell.getType().equals((CellType.WALL)) ||
+                nextCell.getType().equals((CellType.STAIRS)) ||
+                nextCell.getInteractable() != null ||
+                nextCell.getActor() != null) {
+            switchMovementDirection();
+        }
     }
 }
